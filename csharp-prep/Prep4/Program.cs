bb3b090b0e40648d7222389979da4e5f6b4b2560
Kendra Bryant <kbryant@efilecabinet.com>
2023-04-18 15:03:12 -0600

    Completed task 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int num = 0;
+        List<int> numberList = new();
+
+        do
+        {
+            Console.Write("Enter a number. (Type 0 to quit.): ");
+            num = int.Parse(Console.ReadLine());
+            if (num != 0)
+            {
+                numberList.Add(num);
+            }
+        }
+        while (num != 0);
+        
+        int total = numberList.Sum();
+        Console.WriteLine($"\nTotal: {total}");
+
+        double ave = numberList.Average();
+        Console.WriteLine($"Average: {ave}");
+
+        int max = numberList.Max();
+        Console.WriteLine($"Max: {max}");
+
+        int min = numberList.Min();
+        Console.WriteLine($"Min: {min}");
     }
 }
