41741d1e2809229c5f0869c55f7fe55dd6e38507
Kendra Bryant <kbryant@efilecabinet.com>
2023-04-18 15:02:59 -0600

    Completed task 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,64 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gradeInput = Console.ReadLine();
+        int gradePercent = int.Parse(gradeInput);
+        string letter;
+        bool passed = true;
+        string gradeModifier = "";
+
+        char[] gradeChars = gradeInput.ToCharArray();
+        List<int> gradeList = new List<int>();
+
+        foreach (char digit in gradeChars)
+        {
+            gradeList.Add(int.Parse(digit.ToString()));
+        }
+
+        if (gradeList.Count > 1)
+        {
+            if (gradeList[1] >= 7 && gradePercent < 90 && gradePercent >= 60)
+            {
+                gradeModifier = "+";
+            }
+            else if (gradeList[1] <= 3 && gradePercent <= 93 && gradePercent >= 60)
+            {
+                gradeModifier = "-";
+            }
+        }
+
+        if (gradePercent >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercent >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercent >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercent >= 60)
+        {
+            letter = "D";
+            passed = false;
+        }
+        else
+        {
+            letter = "F";
+            passed = false;
+        }
+
+        Console.WriteLine($"Your grade: {letter}{gradeModifier}.");
+
+        if (passed)
+        {
+            Console.WriteLine("Congratulations! You passed!");
+        }
+        else{
+            Console.WriteLine("I'm sorry. You did not pass. Please try again.");
+        }
     }
 }
