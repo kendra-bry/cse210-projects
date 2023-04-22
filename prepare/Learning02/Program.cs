2fb478ed495289bb35ac265be808c398f57e422a
Kendra Bryant <kbryant@efilecabinet.com>
2023-04-21 19:48:43 -0600

    Completed the Resume assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new()
+        {
+            _company = "Adobe",
+            _jobTitle = "Jr. Software Engineer",
+            _startYear = 2015,
+            _endYear = 2017
+        };
+
+        Job job2 = new()
+        {
+            _company = "Entrata",
+            _jobTitle = "Software Engineer",
+            _startYear = 2017,
+            _endYear = 2019
+        };
+
+        Resume resume = new()
+        {
+            _name = "Kenda Bryant",
+            _jobs = new List<Job>
+            {
+                job1,
+                job2
+            }
+        };
+
+        resume.Display();
     }
+
+
+
 }
