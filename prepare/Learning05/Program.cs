688c4ddf2ea505137bc9f644bc42ad2d86a285fb
Kendra Bryant <kbryant@efilecabinet.com>
2023-05-17 20:36:56 -0600

    Shapes program

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,20 @@
-using System;
-
-class Program
+namespace ShapesProgram
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Learning05 World!");
+        static void Main(string[] args)
+        {
+            List<Shape> shapes = new() { };
+            shapes.Add(new Square("Red", 5));
+            shapes.Add(new Rectangle("Pink", 5, 4));
+            shapes.Add(new Square("White", 5));
+
+            foreach (Shape shape in shapes)
+            {
+                Console.WriteLine(shape.GetColor());
+                Console.WriteLine(shape.GetArea());
+            }
+        }
     }
+
 }
